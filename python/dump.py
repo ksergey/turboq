f8c2d6c6b3a9fe94e17620b7c5838ada2376591c
Sergey Kovalevich <inndie@gmail.com>
2025-02-15 13:17:42 +0300

    update

--- python/dump.py
+++
@@ -1,8 +0,0 @@
-import pyturboq
-
-consumer = pyturboq.create_consumer('turboq.spsc')
-
-while True:
-    str = consumer.dequeue()
-    if len(str) > 0:
-        print(str)
--- python/pyturboq/__init__.py
+++
@@ -1,1 +0,0 @@
-from .pyturboq import *
